2d382e289850117e85767fdc8b674404cf944b15
Ben Botto <benjamin.botto@gmail.com>
2019-06-25 13:56:31 -0700

    Adds commands for migrations.

--- src/bin/formn.ts
+++ src/bin/formn.ts
@@ -7,7 +7,20 @@ import { CLIModelGenerator } from '../lib/';
 const argv = yargs
   .strict()
   .usage('Usage: $0 <command>')
+
+  // Generic options.
+  .alias('c', 'connections-file')
+  .nargs('c', 1)
+  .default('c', './connections.json')
+  .describe('c', 'Connections JSON file defining data sources.')
+
+  .alias('f', 'flavor')
+  .nargs('f', 1)
+  .default('f', 'mysql')
+  .describe('f', 'Database flavor.')
+
   .demandCommand(1, 1)
+  // Generate.
   .command(['generate', 'g'], 'Generate entity models.', yargs => {
     return yargs
       .demandCommand(1, 1)
@@ -15,25 +28,45 @@ const argv = yargs
         describe: 'Directory in which entity models shall be saved.',
         type: 'string'
       })
+  })
 
-      .alias('c', 'connections-file')
-      .nargs('c', 1)
-      .default('c', './connections.json')
-      .describe('c', 'Connections JSON file defining data sources.')
+  // Migrate.
+  .command(['migrate', 'm'], 'Migrate a database.', yargs => {
+    return yargs
+      // Generic migrate options.
+      .alias('m', 'migrations-dir')
+      .nargs('m', 1)
+      .default('m', 'migrations')
+      .describe('m', 'Directory in which migration scripts are stored.')
 
-      .alias('f', 'flavor')
-      .nargs('f', 1)
-      .default('f', 'mysql')
-      .describe('f', 'Database flavor.');
+      .demandCommand(1, 1)
+      .command('create <migration-name>', 'Create a new migration.', yargs => {
+        return yargs
+          .positional('migration-name', {
+            description: 'The name of the migration.  It will be prefixed with a timestamp.',
+            type: 'string'
+          })
+      })
+      .command('up', 'Run all new migrations.')
+      .command('down', 'Undo the most recent migration.')
+      .command('run <migration-script>', 'Run a script.', yargs => {
+        return yargs
+          .positional('migration-script', {
+            description: 'A script file to run.',
+            type: 'string'
+          })
+      });
   })
   .argv;
 
 // This is the command (generate, etc.).
 const command = argv._[0];
 
+// Generic options.
+const connFile = argv.c as string;
+const flavor   = argv.f as string;
+
 if (command === 'generate' || command === 'g') {
-  const connFile = argv.c as string;
-  const flavor   = argv.f as string;
   const entDir   = argv._[1];
   const modelGen = new CLIModelGenerator();
 
@@ -43,5 +76,30 @@ if (command === 'generate' || command === 'g') {
       console.error('Error generating models.');
       console.error(err);
     });
+}
+else if (command === 'migrate' || command === 'm') {
+  const migCommand = argv._[1];
+  const migDir     = argv.m as string;
+
+  console.log(`Using migrations directory "${migDir}"`);
+
+  switch (migCommand) {
+    case 'create':
+      const migName = argv.migrationName;
+
+      console.log(`Creating migration: "${migName}"`);
+      break;
+    case 'up':
+      console.log(`Migrating up.`);
+      break;
+    case 'down':
+      console.log(`Migrating down.`);
+      break;
+    case 'run':
+      const migScript = argv.migrationScript;
+
+      console.log(`Running migration script: "${migScript}"`);
+      break;
+  }
 }
 
